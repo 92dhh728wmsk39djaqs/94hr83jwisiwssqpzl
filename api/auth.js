4bb991257d0e08a83c89d55b3dd3da9221f55aee
92dhh728wmsk39djaqs <saripadajalel1@gmail.com>
2026-05-29 18:02:33 +0800

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,25 +1,8 @@
-const crypto = require("crypto");
+export default function handler(req, res) {
 
-module.exports = async (req, res) => {
-  const ua = req.headers["user-agent"] || "";
+    res.setHeader("Content-Type", "text/plain");
 
-  const isRoblox =
-    ua.includes("Roblox") ||
-    req.headers["roblox-id"] ||
-    req.query.source === "roblox";
-
-  if (!isRoblox) {
-    return res.status(403).send("Forbidden");
-  }
-
-  const token = crypto.randomBytes(16).toString("hex");
-
-  res.setHeader("Content-Type", "text/plain");
-  res.setHeader("Cache-Control", "no-store");
-
-  res.send(`
-getgenv().od_token = "${token}"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/prsphecy/flow/refs/heads/main/Initializer.lua"))()
+    res.status(200).send(`loadstring(game:HttpGet("https://raw.githubusercontent.com/prsphecy/flow/refs/heads/main/Initializer.lua"))()
 `);
-};
+
+}
