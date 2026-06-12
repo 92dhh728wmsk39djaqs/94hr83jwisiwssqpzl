666e4c6139891479f88e0f72ba1015f6dd4ddf00
92dhh728wmsk39djaqs <saripadajalel1@gmail.com>
2026-06-12 17:17:02 +0800

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,27 +1,27 @@
 module.exports = function handler(req, res) {
-  const ua = req.headers['user-agent'] || '';
+const ua = req.headers['user-agent'] || '';
 
-  if (ua !== '') {
-    return res.status(403).send('Forbidden');
-  }
+if (ua !== '') {
+return res.status(403).send('Forbidden');
+}
 
-  const script = `
+const script = `
 local requestFunc = syn and syn.request or http and http.request or request
 
 if not requestFunc then
-    return error("Request function not found")
+return error("Request function not found")
 end
 
 local response = requestFunc({
-    Url = "https://raw.githubusercontent.com/prsphecy/flow/refs/heads/main/Initializer.lua",
-    Method = "GET",
-    Headers = {
-        ["x-flow-token"] = "my_9aK2pX_flow_token"
-    }
+Url = "https://raw.githubusercontent.com/prsphecy/flow/refs/heads/main/Initializer.lua",
+Method = "GET",
+Headers = {
+["x-flow-token"] = "my_9aK2pX_flow_token"
+}
 })
 
 loadstring(response.Body)()
 `;
 
-  res.status(200).send(script);
+res.status(200).send(script);
 };
